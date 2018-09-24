28e19a1e0d0d078b2727d3abdeb4bb29bc385ba4
MayBlackwood <ksenia.bobtsova@gmail.com>
2018-09-25 00:58:37 +0300

    First attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr1 = equation.split(' * x^2 ');
+  var a = parseInt(arr1[0]);
+  var arr2 = arr1[1].split(' * x ');
+
+  for (i = 0; i < arr2.length; i++){
+    arr2[i] = arr2[i].replace(" ",""); 
+  }
+    
+  var b = parseInt(arr2[0]);
+  var c = parseInt(arr2[1]);
+  var D = b * b - 4 * a * c;
+  var x1 = Math.round((-b + Math.sqrt(D)) / (2 * a));
+  var x2 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+
+  var answer = [];
+  if ( x1 > x2) { 
+    answer.push(x2);
+    answer.push(x1);  
+    } 
+    else { 
+      answer.push(x1);
+      answer.push(x2);        
+    } 
+    
+ 
+  return answer;
 }
